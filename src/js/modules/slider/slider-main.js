d0efead284aebab1c6692750dd374aa9778b906e
Maria <mari.shumilova.96@mail.ru>
2022-07-27 11:53:49 +0300

    player and sliders

---
+++ src/js/modules/slider/slider-main.js
@@ -0,0 +1,74 @@
+import Slider from "./slider";
+
+export default class MainSlider extends Slider {
+    constructor(page, btns) {
+        super(page, btns);
+    }
+
+    showSlides(n) {
+        if (n > this.slides.length) {
+            this.slideIndex = 1;
+        }
+
+        if (n < 1) {
+            this.slideIndex = this.slides.length;
+        }
+
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch(e){}
+
+        this.slides.forEach(slide => {
+            slide.style.display = "none";
+            slide.classList.remove('animated');
+        });
+
+        this.slides[this.slideIndex - 1].style.display = "block";
+        this.slides[this.slideIndex - 1].classList.add('animated', 'fadeIn');
+
+        //мой вариант
+        // if (n === 3) {
+        //     let windowHanson = this.slides[2].querySelector('.hanson');
+        //     windowHanson.style.display = "none";
+        //     windowHanson.classList.remove('animated');
+        //     setTimeout(() => {
+        //         windowHanson.style.display = 'block';
+        //         windowHanson.classList.add('animated', 'fadeIn');
+        //     }, 3000);
+        // }
+    }
+
+    plusSlides(n) {
+        this.showSlides(this.slideIndex += n);
+    }
+
+    render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch(e){}
+
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                this.plusSlides(1);
+            });
+
+            btn.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            });
+        });
+
+        this.showSlides(this.slideIndex);
+    }
+}
